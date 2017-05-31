e38c65070719ca4e49c7b958752927f5e9288199
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-05-30 22:42:49 -0500

    Adecué archivo js para que el carousel funcione con jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,29 @@
 var target = 0;
 
-var cargarPagina = function () {
+var paginaCargada = function () {
 	// Elementos
-	var botones = document.querySelectorAll(".control");
-	var anterior = document.querySelector(".previous");
-	var siguiente = document.querySelector(".next");
-
+	var $botones = $(".control");
+	var $anterior = $(".previous");
+	var $siguiente = $(".next");
 	// Eventos
-	botones.forEach(function (boton) {
-		boton.addEventListener("click", cambiarImagen);
-	});
-	anterior.addEventListener("click", anteriorImagen);
-	siguiente.addEventListener("click", siguienteImagen);
+	$botones.click(cambiarImagen);
+	$anterior.click(anteriorImagen);
+	$siguiente.click(siguienteImagen);
 };
 
 var cambiarImagen = function () {
-	target = parseInt(this.dataset.target);
+	target = parseInt($(this).data("target"));
 	mostrarImagen(target);
 };
 
 var mostrarImagen = function (target) {
-	var lastSlide = document.querySelector("div.active");
-	var slide = document.querySelector("div[data-slide='" + target + "']");
-	lastSlide.classList.remove("active");
-	slide.classList.add("active");
+	var $lastSlide = $("div.active");
+	var $slide = $("div[data-slide='" + target + "']");
+	$lastSlide.removeClass("active");
+	$slide.addClass("active");
 };
 
+// Esta funcion no se modifica ya que es puro JavaScript
 var anteriorImagen = function (e) {
 	e.preventDefault();
 	target = target - 1;
@@ -33,11 +31,12 @@ var anteriorImagen = function (e) {
 	mostrarImagen(target);
 };
 
+// Esta funcion no se modifica ya que es puro JavaScript
 var siguienteImagen = function (e) {
 	e.preventDefault();
 	target = target + 1;
 	target = (target > 3) ? 0 : target;
 	mostrarImagen(target);mostrarImagen(target);
 };
-
-window.addEventListener("load", cargarPagina);
+// window.addEventListener("load", cargarPagina);
+$(document).ready(paginaCargada);
